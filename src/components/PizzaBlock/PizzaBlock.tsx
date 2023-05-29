cbbb812a14282f03cba02bc8eee826ef602c2fad
magiccwarss <matem.mayreni@gmail.com>
2023-05-29 13:41:50 +0400

    count fixing

--- src/components/PizzaBlock/PizzaBlock.tsx
+++ src/components/PizzaBlock/PizzaBlock.tsx
@@ -1,104 +1,93 @@
-import { FC, useState } from "react";
-import "./PizzaBlock.scss";
-import { useDispatch, useSelector } from "react-redux";
-import { CartItemType, addItem } from "../../redux/slices/cartSlice";
-import { Link } from "react-router-dom";
+import { FC, useState } from 'react';
+import './PizzaBlock.scss';
+import { useDispatch, useSelector } from 'react-redux';
+import { CartItemType, addItem } from '../../redux/slices/cartSlice';
+import { Link } from 'react-router-dom';
 
 type PizzaBlockPropsType = {
-  title: string;
-  price: number;
-  imageUrl: string;
-  sizes: number[];
-  types: number[];
-  id: number;
+   title: string;
+   price: number;
+   imageUrl: string;
+   sizes: number[];
+   types: number[];
+   id: number;
 };
 
-const PizzaBlock: FC<PizzaBlockPropsType> = ({
-  title,
-  price,
-  imageUrl,
-  sizes,
-  types,
-  id,
-}) => {
-  const dispatch = useDispatch();
-  const cartItem = useSelector((state: any) =>
-    state.cart.items.find((obj: any) => obj.id === id)
-  );
-  const addedCount = cartItem ? cartItem.count : 0;
+const PizzaBlock: FC<PizzaBlockPropsType> = ({ title, price, imageUrl, sizes, types, id }) => {
+   const dispatch = useDispatch();
+   const cartItem = useSelector((state: any) =>
+      state.cart.items.find((obj: PizzaBlockPropsType) => obj.id === id),
+   );
+   const addedCount = cartItem ? cartItem.count : 0;
 
-  const [activeType, setActiveType] = useState(0);
-  const [activeSize, setActiveSize] = useState(0);
-  const typeNames = ["thin", "traditional"];
+   const [activeType, setActiveType] = useState(0);
+   const [activeSize, setActiveSize] = useState(0);
+   const [count, setCount] = useState(0);
+   const typeNames = ['thin', 'traditional'];
 
-  const onClickAdd = () => {
-    const item: CartItemType = {
-      id,
-      title,
-      price,
-      imageUrl,
-      type: activeType,
-      size: activeSize,
-      count: 0,
-    };
-    dispatch(addItem(item));
-  };
-  return (
-    <>
-      <div className="pizza-block">
-        <Link to={`pizza/${id}`}>
-          <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
-        </Link>
-        <h4 className="pizza-block__title">{title}</h4>
-        <div className="pizza-block__selector">
-          <ul>
-            {types.map((type) => (
-              <li
-                key={type}
-                onClick={() => setActiveType(type)}
-                className={activeType === type ? "active" : ""}
-              >
-                {typeNames[type]}
-              </li>
-            ))}
-          </ul>
-          <ul>
-            {sizes.map((size, index) => (
-              <li
-                key={size}
-                onClick={() => setActiveSize(index)}
-                className={activeSize === index ? "active" : ""}
-              >
-                {size} cm.
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="pizza-block__bottom">
-          <div className="pizza-block__price">from {price} AMD</div>
-          <button
-            onClick={onClickAdd}
-            className="button button--outline button--add"
-          >
-            <svg
-              width="12"
-              height="12"
-              viewBox="0 0 12 12"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
-                fill="white"
-              />
-            </svg>
-            <span>Add</span>
-            <i>{addedCount}</i>
-          </button>
-        </div>
-      </div>
-    </>
-  );
+   const onClickAdd = () => {
+      const item: CartItemType = {
+         id,
+         title,
+         price,
+         imageUrl,
+         type: activeType,
+         size: activeSize,
+         count: 0,
+      };
+      dispatch(addItem(item));
+      setCount((prevCount) => prevCount + 1);
+   };
+   return (
+      <>
+         <div className="pizza-block">
+            <Link to={`pizza/${id}`}>
+               <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
+            </Link>
+            <h4 className="pizza-block__title">{title}</h4>
+            <div className="pizza-block__selector">
+               <ul>
+                  {types.map((type) => (
+                     <li
+                        key={type}
+                        onClick={() => setActiveType(type)}
+                        className={activeType === type ? 'active' : ''}>
+                        {typeNames[type]}
+                     </li>
+                  ))}
+               </ul>
+               <ul>
+                  {sizes.map((size, index) => (
+                     <li
+                        key={size}
+                        onClick={() => setActiveSize(index)}
+                        className={activeSize === index ? 'active' : ''}>
+                        {size} cm.
+                     </li>
+                  ))}
+               </ul>
+            </div>
+            <div className="pizza-block__bottom">
+               <div className="pizza-block__price">from {price} AMD</div>
+               <button onClick={onClickAdd} className="button button--outline button--add">
+                  <svg
+                     width="12"
+                     height="12"
+                     viewBox="0 0 12 12"
+                     fill="none"
+                     xmlns="http://www.w3.org/2000/svg">
+                     <path
+                        d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+                        fill="white"
+                     />
+                  </svg>
+                  <span>Add</span>
+                  <i>{count}</i>
+               </button>
+            </div>
+         </div>
+      </>
+   );
 };
 
 export default PizzaBlock;
