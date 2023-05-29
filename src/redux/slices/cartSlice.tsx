b54bcf85b120b92e58a80d5b0a5c3129ae9a7721
magiccwarss <matem.mayreni@gmail.com>
2023-05-29 13:07:30 +0400

    fixing diffrent types pizzas bags

--- src/redux/slices/cartSlice.tsx
+++ src/redux/slices/cartSlice.tsx
@@ -26,43 +26,59 @@ const cartSlice = createSlice({
    initialState,
    reducers: {
       addItem(state, action: PayloadAction<CartItemType>) {
-         const findItem = state.items.find((obj) => obj.id === action.payload.id);
-         if (findItem) {
-            findItem.count++;
+         const newItem = {
+            ...action.payload,
+            count: 1,
+         };
+
+         const existingItemIndex = state.items.findIndex(
+            (item) =>
+               item.id === newItem.id && item.type === newItem.type && item.size === newItem.size,
+         );
+
+         if (existingItemIndex !== -1) {
+            state.items[existingItemIndex].count++;
          } else {
-            state.items.push({
-               ...action.payload,
-               count: 1,
-            });
+            state.items.push(newItem);
          }
-         state.totalPrice = state.items.reduce((sum, obj) => {
-            return obj.price * obj.count + sum;
-         }, 0);
+
+         state.totalPrice = calculateTotalPrice(state.items);
       },
       minusItem(state, action: PayloadAction<CartItemType>) {
-         const findItem = state.items.find((obj) => obj.id === action.payload.id);
-         if (findItem) {
-            if (findItem.count >= 1) {
-               findItem.count--;
+         const existingItemIndex = state.items.findIndex(
+            (item) =>
+               item.id === action.payload.id &&
+               item.type === action.payload.type &&
+               item.size === action.payload.size,
+         );
+
+         if (existingItemIndex !== -1) {
+            if (state.items[existingItemIndex].count > 1) {
+               state.items[existingItemIndex].count--;
             } else {
-               state.items = state.items.filter((obj) => obj.id !== action.payload.id);
+               state.items.splice(existingItemIndex, 1);
             }
-            state.totalPrice = state.items.reduce((sum, obj) => {
-               return obj.price * obj.count + sum;
-            }, 0);
-         } else {
-            // Item not found
          }
+
+         state.totalPrice = calculateTotalPrice(state.items);
       },
 
       removeItems(state, action: PayloadAction<number>) {
-         state.items = state.items.filter((obj) => obj.id !== action.payload);
+         state.items = state.items.filter((item) => item.id !== action.payload);
+         state.totalPrice = calculateTotalPrice(state.items);
       },
       clearItems(state) {
          state.items = [];
+         state.totalPrice = 0;
       },
    },
 });
+
+const calculateTotalPrice = (items: CartItemType[]) => {
+   return items.reduce((sum, item) => {
+      return item.price * item.count + sum;
+   }, 0);
+};
 
 export const selectCart = (state: RootState) => state.cart;
 export default cartSlice.reducer;
